b3bea46e42c46fec5ed28787f6a0005fc44581d7
averywallis <averybwallis@gmail.com>
2016-02-01 14:05:31 -0500

    Updated from Brython Server: 2/1/2016 2:05:31 PM

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
 """
 birthday.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Avery Wallis
+Credit: None so far
 Assignment:
 
 Your program will ask the user the following questions, in this order:
@@ -12,19 +12,12 @@ Your program will ask the user the following questions, in this order:
 4. The day they were born on (e.g. "11").
 
 If the user's birthday fell on October 31, then respond with:
-
   You were born on Halloween!
-
 If the user's birthday fell on today's date, then respond with:
-
   Happy birthday!
-
 Otherwise respond with a statement like this:
-
   Peter, you are a winter baby of the nineties.
-
-Example Session
-
+Example Session:
   Hello, what is your name? Eric
   Hi Eric, what was the name of the month you were born in? September
   And what year were you born in, Eric? 1972
